3e883bfa6c07f476c1535e6191cb54bc976d2974
Jeremiah DeGreeff <degreeff@mit.edu>
2023-11-11 22:53:53 -0500

    don't store odd numbers

--- PrimeJulia/solution_5/PrimeSieve.jl
+++ PrimeJulia/solution_5/PrimeSieve.jl
@@ -1,3 +1,6 @@
+div2(x) = x >> 1
+mul2(x) = x << 1
+
 const validation_dict = Dict{Int64, Int64}(
     10 => 4,
     100 => 25,
@@ -13,23 +16,20 @@ struct Sieve
     size::Int64
     bits::BitVector
 
-    Sieve(size::Int64) = new(size, trues(size))
+    Sieve(size::Int64) = new(size, trues(div2(size)))
 end
 
 function run_sieve!(sieve::Sieve)
-    sieve.bits[1] = false
-    sieve.bits[4:2:sieve.size] .= false
-    
     factor = 3
     q = ceil(Int64, sqrt(sieve.size))
 
     while factor < q
-        for num in (factor * factor):2factor:sieve.size
-            sieve.bits[num] = false
+        for index in (div2(factor * factor)+1):factor:div2(sieve.size)
+            sieve.bits[index] = false
         end
-        for num in (factor+2):2:sieve.size
-            if sieve.bits[num]
-                factor = num
+        for index in (div2(factor)+2):div2(sieve.size)
+            if sieve.bits[index]
+                factor = mul2(index) - 1
                 break
             end
         end
@@ -40,7 +40,7 @@ count_primes(sieve::Sieve) = sum(sieve.bits)
 validate(sieve::Sieve) = haskey(validation_dict, sieve.size) && validation_dict[sieve.size] == count_primes(sieve)
 
 function printResults(sieve::Sieve, show_results::Bool, duration::Float64, passes::Int64)
-    show_results && println(join((i for (i, b) in enumerate(sieve.bits) if b), ", "))
+    show_results && println(join(replace([2i - 1 for (i, b) in enumerate(sieve.bits) if b], 1 => 2), ", "))
     
     println("Passes: $passes, Time: $(duration)s, Avg: $(duration / passes)s, Limit: $(sieve.size), Count: $(count_primes(sieve)), Valid: $(validate(sieve))")
     println()
